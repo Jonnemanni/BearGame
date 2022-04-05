a0ff26df870ac136f8019642710eeb6986d44d0d
Jonnemanni <joni9909@hotmail.fi>
2022-04-05 15:11:00 +0300

    Player no longer slides when coming to a stop.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,32 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
-    // Getting Rigidbody2D under the name RB.
-    private Rigidbody2D rb;
-    // Getting the Animator under the name Anim.
-    private Animator anim;
-    // Walk Speed variable for easy editing.
     public int walkSpeed;
     // Jump Height variable for easy editing.
     public int jumpHeight;
+    // Getting Rigidbody2D under the name RB.
+    [SerializeField] private Rigidbody2D rb;
+    // Getting Collider2d under the name Collider.
+    [SerializeField] private Collider2D collider;
+    // Getting the Animator under the name Anim.
+    [SerializeField] private Animator anim;
+    // Walk Speed variable for easy editing.
     // Enumerating states.
     private enum State {idle, run, jump};
-    private State state = State.idle;
+    [SerializeField] private State state = State.idle;
+    // Direction
+    [SerializeField] private float hDirection;
+    [SerializeField] private float vDirection;
+    // Ground Layer
+    [SerializeField] private LayerMask ground;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {   
-        float hDirection = Input.GetAxis("Horizontal");
-        float vDirection = Input.GetAxis("Vertical");
+        hDirection = Input.GetAxis("Horizontal");
         // Moving Right
         if (hDirection>0) 
         {
@@ -43,14 +51,13 @@ public class PlayerController : MonoBehaviour
         // Stopped
         else
         {
-
+            rb.velocity = new Vector2(0,rb.velocity.y);
         }
 
         // Jump
-        if (vDirection>0)
+        if (Input.GetButtonDown("Jump") && collider.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpHeight);
-            state = State.jump;
         }
 
         velocityState();
@@ -59,9 +66,9 @@ public class PlayerController : MonoBehaviour
 
     private void velocityState()
     {
-        if(state == State.jump)
+        if(vDirection!=0)
         {
-
+            state = State.jump;
         }
         else if(Mathf.Abs(rb.velocity.x) > 2f)
         {
